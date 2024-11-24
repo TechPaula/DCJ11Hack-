6a76ecd943b14b3cb73373f60f9597a68172fd18
TechPaula <Paula@synth.net>
2024-11-24 21:13:54 +0000

    fixed a number of bugs and removed the use of r6 in places for JSR

--- Example Code/GoL.asm
+++ Example Code/GoL.asm
@@ -14,7 +14,7 @@
 	; .blkb 76 sets the start of code at o000100 
 	;
 	; r0 -> r5 general purpose registers
-	; r6  normally used for stack pointer, but can be used as general purpose
+	; r6  normally used for stack pointer, when I use it, things don't work.
 	; r7  program counter
 	;
 	; Memory map
@@ -49,22 +49,26 @@ VLBL: .word 000000			; value of square bottom left
 VLBC: .word 000000			; value of square bottom center
 VLBR: .word 000000			; value of square bottom right
 ITER: .word 000000			; number of iterations
+TMP:  .word 000000			; temporary storage
 
 
 .org 003000
 INIT:					; should be at o003000
 	clr r0
+	clr r1
 	clr r2
 	clr r3
 	clr r4
 	clr r5
-	clr r6				; used for JSR/RTS
+	MOV #000076,r6		; STACK POINTER set (note it goes DOWN as it fills)
 
-	jsr r6, MDISP		; show start message
+	jsr r5, MDISP		; show start message
 	
-	jsr r6, SETUP		; set initial memory values
+	jsr r5, SETUP		; set initial memory values
+
+	jsr r5, SHOWR		; Show results on 16x16 matrix
 	
-	jsr r6, SHOWR		; Show results on 16x16 matrix
+	
 
 ;-----------------------------------------------------
 ; main loop
@@ -160,10 +164,18 @@ MULE:					; if it is, we're done
 
 ; ----------------------------------------------------
 ; Show result on matrix
-; 	trashes r0, r1, r2, r3, r4, r5
+; 	trashes r0, r1, r2, r3, r4
+;		r0 = value for 8bit column data
+;		r1 = working refister
+;		r2 = number of columns
+;		r3 = number  of columns complete
+;		r4 = address in results page
+;		//r5 = address of dot matrix
 SHOWR:
 	; convert contents of RAM (r4) to display (r5)
-	mov #140000, r5	; dot display address
+	mov #TMP, r3
+	mov #140000, (r3)	; save 16x16 display address in temp 
+	
 	mov #RSLT, r4 	; results page address
 	clr r3			; number of columns done
 	clr r2			; number of sets of columns
@@ -225,10 +237,19 @@ SKP7:
 	add #000200, r0	; set bit in r0
 	
 DILE:
-	mov r0, (r5)	; put it in the display
+	mov #TMP, r1	; put the TMP address into r1 
+	mov (r1),r1
+
+	mov r0, (r1)	; put it in the display
 	sub #000016, r4	; take off all the addition we did
 	add #000040, r4	; go to next address
-	add #2, r5		; point display to next address
+	
+	mov #TMP, r1	; put the TMP address into r1 
+	mov (r1),r0		; take contents of address held by r1 and put it in r0
+	add #2, r0		; point display to next address
+	mov r0, (r1)	; put the new address back in temp
+	
+	halt
 
 	inc r3			; increase column counter
 	cmp r3, #000020	; have we done 32 (2x16 sets of 8 bits)
@@ -240,61 +261,56 @@ DILE:
 	cmp r2, #000002
 	blt DILP
 	
-	rts r6
+	rts r5
 
 	
 ; ----------------------------------------------------
 ; setup memory
-;	trashes r0, r3, r5
+;	trashes r0, r1, r3
 SETUP:
-	clr r3
+	clr r3					; clear loop counter
+	mov #RSLT, r1			; mov result memory location into r5
+	mov #ISTP, r0			; start matrix address
+	
 ILOP:
-	mov (r0), (r5)			; copy contents of  address R0 to contents of address r5
-	inc r3
+	mov (r0), (r1)			; copy contents of  address R0 to contents of address r1
+	inc r3					; increase loop counter
 	add #2, r0				; increase pointer
-	add #2, r5				; increase pointer
+	add #2, r1				; increase pointer
 	cmp r3, #000400			; have we done 256 addresses?
 	blt ILOP
 
 
 	clr r3
 CLRL:						; clear next 256 locations
-	mov #000000, (r5)		; Make sure it's all zero
+	mov #000000, (r1)		; Make sure it's all zero
 	inc r3
 	add #2, r0				; increase pointer
-	add #2, r5				; increase pointer
+	add #2, r1				; increase pointer
 	cmp r3, #000400			; have we done 256 addresses?
 	blt CLRL
 	
-
-
-	rts r6
+	rts r5
 
 
 ; ----------------------------------------------------
 ; show display message
-;	trashes r0,r3,r5
+;	trashes r0,r1,r3
 MDISP:
 	; display message
-	mov #060036, r5
-	mov #MESG, r0
-	clr r3
+	mov #060036, r1			; Display address (left most character)
+	mov #MESG, r0			; Start message
+	clr r3					; loop counter
 
 MLOP:
-	mov (r0), (r5)			; copy contents of  address R0 to contents of address r5
-	inc r3
+	mov (r0), (r1)			; copy contents of address held in R0 to contents of address held in r1
+	inc r3					; increase loop counter
 	add #2, r0				; increase pointer
-	sub #2, r5				; increase pointer
+	sub #2, r1				; increase pointer
 	cmp r3, #000020			; have we done 16 characters?
 	blt MLOP
 
-
-	; put first step into RAM
-	mov #000100, r5			; 000010 is first page of GoL memory
-	mov #ISTP, r0			; the start of the message
-	clr r3
-
-	rts r6
+	rts r5
 	
 
 ; ----------------------------------------------------
